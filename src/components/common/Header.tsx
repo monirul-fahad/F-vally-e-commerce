d10a8b858e549b6ac00b15c4fa1e1618e063df50
monirul-fahad <immonirulfahad@gmail.com>
2022-01-18 21:43:15 +0600

    scss added

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { FiPhoneCall } from 'react-icons/fi';
+import { FaRegEnvelope } from 'react-icons/fa';
+import { BsPhone } from 'react-icons/bs';
 
 const Header = () => {
   return (
-    <div>
+    <div className="header__component">
+      <div className="top-header">
+        <Container className="d-flex align-items-center justify-content-between">
+          <ul className="list-unstyled d-flex align-items-center gap-4">
+            <li>
+              <FiPhoneCall /> <a href="tel:01879664431">01879664431</a>
+            </li>
+            <li>
+              <FaRegEnvelope />{' '}
+              <a href="mail-to:immonirulfahad@gmail.com">
+                immonirulfahad@gmail.com
+              </a>
+            </li>
+          </ul>
+          <div>
+            <BsPhone />
+            <a href="#">Save big on our app!</a>
+          </div>
+        </Container>
+      </div>
       <Navbar bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
